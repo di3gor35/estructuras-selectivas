d8a3964ef33343f0a285b84311416120a6cdf47f
di3go <djramosm@unjbg.edu.pe>
2024-06-15 12:59:02 -0500

    Actualizando ejercicio 1: se agregó una funcion la cual procesa el cálculo del precio final

--- selec1.cpp
+++ selec1.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+
+float Calcular_PrecioFinal(float p_inicial, int n){
+	// Calcular el precio final
+	if (n >= 3){
+		return p_inicial - (0.2 * p_inicial);
+	} else {
+		return p_inicial - (0.1 * p_inicial);
+	}
+}
+
+int main(){
 	float p_compra, p_final;
 	int n_camisas;
 	cout << "Bienvenido al programa, para calcular el precio final" << endl;
@@ -9,15 +18,7 @@ int main()
 	cin >> p_compra;
 	cout << "Ingrese el numero de camisas: ";
 	cin >> n_camisas;
-	if (n_camisas >= 3)
-	{
-		p_final = p_compra - (0.2 * p_compra);
-		cout << "El precio final es: " << p_final;
-	}
-	else
-	{
-		p_final = p_compra - (0.1 * p_compra);
-		cout << "El precio final es: " << p_final;
-	}
+	p_final = Calcular_PrecioFinal(p_compra, n_camisas);
+	cout << "El precio final es: " << p_final;
 	return 0;
 }
