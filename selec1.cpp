94741d7e8287e793520a408ac213177a9148d43d
di3go <djramosm@unjbg.edu.pe>
2024-06-16 15:10:25 -0500

    Actualizando ejercicio 1: se agregó una condición que anticipa valores inválidos y devuelve mensaje de error

--- selec1.cpp
+++ selec1.cpp
@@ -13,12 +13,16 @@ float Calcular_PrecioFinal(float p_inicial, int n){
 int main(){
 	float p_compra, p_final;
 	int n_camisas;
-	cout << "Bienvenido al programa, para calcular el precio final" << endl;
+	cout << "Bienvenido al programa, para calcular el precio final de una compra" << endl;
 	cout << "Ingrese el precio de la compra: ";
 	cin >> p_compra;
 	cout << "Ingrese el numero de camisas: ";
 	cin >> n_camisas;
-	p_final = Calcular_PrecioFinal(p_compra, n_camisas);
-	cout << "El precio final es: " << p_final;
+	if (p_compra <= 0 || n_camisas <= 0){
+		cout << "Error, las cantidades deben ser mayores a cero";
+	} else {
+		p_final = Calcular_PrecioFinal(p_compra, n_camisas);
+		cout << "El precio final es: " << p_final;
+	}
 	return 0;
 }
