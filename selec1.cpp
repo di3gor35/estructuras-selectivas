5006c6c40de9ba7c72091b3e50839db55170c893
di3go <djramosm@unjbg.edu.pe>
2024-06-17 11:37:43 -0500

    Actualizando ejercicio 1: se agregó un bucle el cual vuelve a repetir el programa si el cliente lo diga

--- selec1.cpp
+++ selec1.cpp
@@ -13,16 +13,21 @@ float Calcular_PrecioFinal(float p_inicial, int n){
 int main(){
 	float p_compra, p_final;
 	int n_camisas;
+	bool res;
 	cout << "Bienvenido al programa, para calcular el precio final de una compra" << endl;
-	cout << "Ingrese el precio de la compra: ";
-	cin >> p_compra;
-	cout << "Ingrese el numero de camisas: ";
-	cin >> n_camisas;
-	if (p_compra <= 0 || n_camisas <= 0){
-		cout << "Error, las cantidades deben ser mayores a cero";
-	} else {
-		p_final = Calcular_PrecioFinal(p_compra, n_camisas);
-		cout << "El precio final es: " << p_final;
-	}
+	do {
+		cout << "Ingrese el precio de la compra: ";
+		cin >> p_compra;
+		cout << "Ingrese el numero de camisas: ";
+		cin >> n_camisas;
+		if (p_compra <= 0 || n_camisas <= 0){
+			cout << "Error, las cantidades deben ser mayores a cero" << endl;
+			cout << "Desea intentar de nuevo? (1: si/ 0: no): ", cin >> res;
+		} else {
+			p_final = Calcular_PrecioFinal(p_compra, n_camisas);
+			cout << "El precio final es: " << p_final << endl;
+			cout << "Desea hacer otra consulta? (1: si/ 0: no): ", cin >> res;
+		}
+	} while (res == 1);
 	return 0;
 }
