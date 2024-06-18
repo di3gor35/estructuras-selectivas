068e39f2a795d10e803ea621262fc95673d51262
di3go <djramosm@unjbg.edu.pe>
2024-06-18 11:58:28 -0500

    Actualizando ejercicio 1: se agregó una función que valida las respuestas del usuario y dos bucles para buscar una respuesta válida

--- selec1.cpp
+++ selec1.cpp
@@ -9,11 +9,21 @@ float Calcular_PrecioFinal(float p_inicial, int n){
 		return p_inicial - (0.1 * p_inicial);
 	}
 }
+bool Validar_Respuesta(int r){
+	if (r == 0 || r == 1){
+		return true;
+	}
+	else {
+		cout << "Opcion no valida" << endl;
+		return false;
+	}
+}
 
 int main(){
 	float p_compra, p_final;
 	int n_camisas;
-	bool res;
+	int res;
+	bool r;
 	cout << "Bienvenido al programa, para calcular el precio final de una compra" << endl;
 	do {
 		cout << "Ingrese el precio de la compra: ";
@@ -22,12 +32,20 @@ int main(){
 		cin >> n_camisas;
 		if (p_compra <= 0 || n_camisas <= 0){
 			cout << "Error, las cantidades deben ser mayores a cero" << endl;
+			do {
 			cout << "Desea intentar de nuevo? (1: si/ 0: no): ", cin >> res;
+			r = Validar_Respuesta(res);
+			} while (r == false);
+
 		} else {
 			p_final = Calcular_PrecioFinal(p_compra, n_camisas);
 			cout << "El precio final es: " << p_final << endl;
+			do {
 			cout << "Desea hacer otra consulta? (1: si/ 0: no): ", cin >> res;
+			r = Validar_Respuesta(res);
+			} while (r == false);
+
 		}
-	} while (res == 1);
+	} while (res == true);
 	return 0;
 }
